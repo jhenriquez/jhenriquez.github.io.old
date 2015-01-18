6f6ea4962c1165caa21666adfc20630faba104b1
Julio Henriquez <julio.m.henriquez@gmail.com>
2015-01-18 11:55:28 -0400

    Added circules as nav. Repositioned to the left.

--- scripts/views/index.js
+++ scripts/views/index.js
@@ -1,9 +1,13 @@
 requirejs(['../main'], function() {
   requirejs(['jquery'], function () {
     requirejs(['bootstrap', 'fullpage'], function () {
+
       $('#content').fullpage({
-        sectionsColor: ['#82938A', '#C3CBAC', '#4A4A45']
+        sectionsColor: ['#82938A', '#C3CBAC', '#4A4A45'],
+        anchors: ['Section1', 'Section2', 'Section3'],
+        menu: '#menu'
       });
+
     });
   });
 });
