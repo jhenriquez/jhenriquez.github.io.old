31f8dcf15e857adcd9017f24226209d6ada0b32c
Julio Henriquez <julio.m.henriquez@gmail.com>
2015-02-25 16:40:03 -0400

    Added basic layout styling to LastSong component.

--- _react/LastSong.js
+++ _react/LastSong.js
@@ -5,28 +5,53 @@ define(['react', 'jquery'], function (React) {
       return null;
     },
 
+    HandleResult: function (data) {
+      var self = this;
+      var state = { };
+
+      (function (tr) {
+        state = {
+          name: tr.name,
+          artist: tr.artist["#text"],
+          album: tr.album['#text'] || 'N/A',
+          image: tr.image[tr.image.length-1]['#text'] || self.props.defaultImage || '',
+          url: tr.url
+        };
+
+        self.setState(state);
+
+        if (state.image) return;
+
+        $.get('http://ws.audioscrobbler.com/2.0/', { 
+            method: 'artist.getInfo',
+            artist: state.artist,
+            api_key: self.props.apiKey,
+            format: 'json'
+            },
+            function (data) {
+              (function (a) {
+                state.image = a.image[a.image.length-1]['#text'];
+                self.setState(state);
+              })(data.artist);
+            }
+        );
+        
+      })(data.recenttracks.track[0]);
+    },
+
     getLastSong: function () {
       $.get('http://ws.audioscrobbler.com/2.0/', {
           method: 'user.getrecenttracks',
           user: this.props.username,
           api_key: this.props.apiKey,
+          nowplaying: false,
           format: 'json'
-        },
-        function (data, status) {
-          (function (tr) {
-            this.setState({
-              name: tr.name,
-              album: tr.album['#text'] || 'N/A',
-              image: tr.image[tr.image.length-1]['#text'] || this.props.defaultImage,
-              url: tr.url
-            });
-          })(data.recenttracks.track[1]);
-        });
+        }, this.HandleResult);
     },
 
     componentDidMount: function () {
       this.getLastSong();
-      setInterval(this.getLastSong, 60000);
+      setInterval(this.getLastSong, 30000);
     },
  
     render: function () {
@@ -37,13 +62,26 @@ define(['react', 'jquery'], function (React) {
       return (
         <div className="last-song-container">
           <img src={this.state.image} className="last-song-image" />
-          <div className="last-song-name">{this.state.name}</div>
-          <div className="last-song-album">{this.state.album}</div>
+          <div className="last-song-content">
+            <div className="last-song-name">
+              <span className="last-song-tag">Title:</span>{this.state.name}
+            </div>
+            <div className="last-song-album">
+              <span className="last-song-tag">Album:</span>{this.state.album}
+            </div>
+            <div className="last-song-artist">
+              <span className="last-song-tag">By:</span>{this.state.artist}
+            </div>
+            <div className="last-song-powerby">
+              <a href={this.state.url}>
+                <i className="fa fa-lastfm-square"></i>
+              </a>
+            </div>
+          </div>
         </div>
         );
     }
   });
 
   return LastSong;
-
 });
--- scripts/views/components/LastSong.js
+++ scripts/views/components/LastSong.js
@@ -5,28 +5,53 @@ define(['react', 'jquery'], function (React) {
       return null;
     },
 
+    HandleResult: function (data) {
+      var self = this;
+      var state = { };
+
+      (function (tr) {
+        state = {
+          name: tr.name,
+          artist: tr.artist["#text"],
+          album: tr.album['#text'] || 'N/A',
+          image: tr.image[tr.image.length-1]['#text'] || self.props.defaultImage || '',
+          url: tr.url
+        };
+
+        self.setState(state);
+
+        if (state.image) return;
+
+        $.get('http://ws.audioscrobbler.com/2.0/', { 
+            method: 'artist.getInfo',
+            artist: state.artist,
+            api_key: self.props.apiKey,
+            format: 'json'
+            },
+            function (data) {
+              (function (a) {
+                state.image = a.image[a.image.length-1]['#text'];
+                self.setState(state);
+              })(data.artist);
+            }
+        );
+        
+      })(data.recenttracks.track[0]);
+    },
+
     getLastSong: function () {
       $.get('http://ws.audioscrobbler.com/2.0/', {
           method: 'user.getrecenttracks',
           user: this.props.username,
           api_key: this.props.apiKey,
+          nowplaying: false,
           format: 'json'
-        },
-        function (data, status) {
-          (function (tr) {
-            this.setState({
-              name: tr.name,
-              album: tr.album['#text'] || 'N/A',
-              image: tr.image[tr.image.length-1]['#text'] || this.props.defaultImage,
-              url: tr.url
-            });
-          })(data.recenttracks.track[1]);
-        });
+        }, this.HandleResult);
     },
 
     componentDidMount: function () {
       this.getLastSong();
-      setInterval(this.getLastSong, 60000);
+      setInterval(this.getLastSong, 30000);
     },
  
     render: function () {
@@ -37,13 +62,26 @@ define(['react', 'jquery'], function (React) {
       return (
         React.createElement("div", {className: "last-song-container"}, 
           React.createElement("img", {src: this.state.image, className: "last-song-image"}), 
-          React.createElement("div", {className: "last-song-name"}, this.state.name), 
-          React.createElement("div", {className: "last-song-album"}, this.state.album)
+          React.createElement("div", {className: "last-song-content"}, 
+            React.createElement("div", {className: "last-song-name"}, 
+              React.createElement("span", {className: "last-song-tag"}, "Title:"), this.state.name
+            ), 
+            React.createElement("div", {className: "last-song-album"}, 
+              React.createElement("span", {className: "last-song-tag"}, "Album:"), this.state.album
+            ), 
+            React.createElement("div", {className: "last-song-artist"}, 
+              React.createElement("span", {className: "last-song-tag"}, "By:"), this.state.artist
+            ), 
+            React.createElement("div", {className: "last-song-powerby"}, 
+              React.createElement("a", {href: this.state.url}, 
+                React.createElement("i", {className: "fa fa-lastfm-square"})
+              )
+            )
+          )
         )
         );
     }
   });
 
   return LastSong;
-
 });
