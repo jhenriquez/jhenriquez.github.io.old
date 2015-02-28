865581401a79825acd9703b486f81ee852e38aeb
Julio Henriquez <julio.m.henriquez@gmail.com>
2015-02-28 12:50:27 -0400

    Added support for multiple element toggling

--- scripts/views/components/MenuToggle.js
+++ scripts/views/components/MenuToggle.js
@@ -6,9 +6,9 @@ define(function () {
     this.toggleClass = toggleClass;    
 
     if (typeof toggleElement === 'string') {
-      this.toggleElement = document.querySelector(toggleElement);
+      this.toggleElement = document.querySelectorAll(toggleElement);
 
-      if (!this.toggleElement) {
+      if (!this.toggleElement || this.toggleElement.length === 0) {
         throw new Error('The provided toggleElement selector did not match any element.');
       }
     } else {
@@ -25,7 +25,9 @@ define(function () {
     }
 
     this.toggleButtom.addEventListener('click', function (e) {
-        self.toggleElement.classList.toggle(self.toggleClass);
+        for(var i = 0; i < self.toggleElement.length; i++) {
+          self.toggleElement[i].classList.toggle(self.toggleClass);
+        }
     }, false);
   }
 
